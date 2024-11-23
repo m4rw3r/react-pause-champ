fceed32eeca6a77845f2d1b39f4f97773e9ba7e3
Martin Wernstål <m4rw3r@gmail.com>
2024-11-23 20:01:04 +0100

    feat: added multiple react-versions for testing

--- packages/react-pause-champ/jest.config.ts
+++ packages/react-pause-champ/jest.config.ts
@@ -1,9 +1,18 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 import defaultConfig from "../../jest.config";
 
+const moduleNameMapper: Record<string, string> = {};
+
+if (process.env.REACT_VERSION) {
+  moduleNameMapper["^react((\\/.*)?)$"] = `react${process.env.REACT_VERSION}$1`;
+  moduleNameMapper["^react-dom((\\/.*)?)$"] =
+    `react${process.env.REACT_VERSION}-dom$1`;
+}
+
 const jestConfig: JestConfigWithTsJest = {
   ...defaultConfig,
   testEnvironment: "jsdom",
+  moduleNameMapper,
 };
 
 export default jestConfig;
